375e1fa15447a0f3a97507168e3a5ed213a4db87
paerab@gmail.com <paerab@gmail.com>
2022-12-25 15:38:53 +0500

    passed tests load/write

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
@@ -36,8 +36,14 @@ public final class WebCrawlerMain {
 
     CrawlResult result = crawler.crawl(config.getStartPages());
     CrawlResultWriter resultWriter = new CrawlResultWriter(result);
-    // TODO: Write the crawl results to a JSON file (or System.out if the file name is empty)
-    // TODO: Write the profile data to a text file (or System.out if the file name is empty)
+    String path = config.getResultPath();
+    if (path.isEmpty()){
+      Writer writerStream  = new OutputStreamWriter(System.out);
+      resultWriter.write(writerStream);
+    }
+    else{
+      resultWriter.write(Path.of(path));
+    }
   }
 
   public static void main(String[] args) throws Exception {
