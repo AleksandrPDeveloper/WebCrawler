40cbcdad4a739b0b0aaef121266a2dfa18f0a33c
paerab@gmail.com <paerab@gmail.com>
2023-01-13 00:44:09 +0500

    pass profilers tests

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
@@ -1,8 +1,11 @@
 package com.udacity.webcrawler.profiler;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.time.Clock;
+import java.time.Duration;
+import java.time.ZonedDateTime;
 import java.util.Objects;
 
 /**
@@ -11,20 +14,36 @@ import java.util.Objects;
  */
 final class ProfilingMethodInterceptor implements InvocationHandler {
 
-  private final Clock clock;
+    private final Clock clock;
+    private final ProfilingState state;
+    private final Object delegate;
 
-  // TODO: You will need to add more instance fields and constructor arguments to this class.
-  ProfilingMethodInterceptor(Clock clock) {
-    this.clock = Objects.requireNonNull(clock);
-  }
 
-  @Override
-  public Object invoke(Object proxy, Method method, Object[] args) {
-    // TODO: This method interceptor should inspect the called method to see if it is a profiled
-    //       method. For profiled methods, the interceptor should record the start time, then
-    //       invoke the method using the object that is being profiled. Finally, for profiled
-    //       methods, the interceptor should record how long the method call took, using the
-    //       ProfilingState methods.
-    return null;
-  }
+    ProfilingMethodInterceptor(Clock clock, ProfilingState state, Object delegate) {
+        this.clock = Objects.requireNonNull(clock);
+        this.state = state;
+        this.delegate = Objects.requireNonNull(delegate);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Object obj;
+        if (method.isAnnotationPresent(Profiled.class)) {
+            ZonedDateTime startTime = ZonedDateTime.now(clock);
+            try {
+                obj = method.invoke(delegate, args);
+                state.record(delegate.getClass(), method, Duration.between(startTime, ZonedDateTime.now(clock)));
+                return obj;
+            } catch (InvocationTargetException e) {
+                state.record(delegate.getClass(), method, Duration.between(startTime, ZonedDateTime.now(clock)));
+                throw e.getTargetException();
+            }
+        } else {
+            try {
+                return method.invoke(delegate, args);
+            } catch (InvocationTargetException e) {
+                throw e.getTargetException();
+            }
+        }
+    }
 }
